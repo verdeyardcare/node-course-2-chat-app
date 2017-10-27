b2c7379e28f8b02e5b0f8fb9dd726ea1c73d84ef
verdeyardcare <30375500+verdeyardcare@users.noreply.github.com>
2017-10-27 15:55:05 -0700

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,20 @@
  var socket = io();
+
+function scrollToBottom() {
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child')
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+    
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+};
         
 socket.on('connect', function (){
             console.log('Connected to server');
@@ -20,6 +36,7 @@ socket.on('newMessage', function(message) {
     });
     
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 
@@ -34,7 +51,7 @@ socket.on('newLocationMessage', function (message) {
     });
     
     jQuery('#messages').append(html);
-
+    scrollToBottom();
 });
 var messageTextbox =  jQuery('[name=message]')
 
