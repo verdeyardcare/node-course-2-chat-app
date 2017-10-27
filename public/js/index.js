e6aa1295683ffa7b1668e972fd87f237eedd919f
verdeyardcare <30375500+verdeyardcare@users.noreply.github.com>
2017-10-27 14:12:14 -0700

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -26,15 +26,16 @@ socket.on('newLocationMessage', function (message) {
     li.append(a);
     jQuery('#messages').append(li);
 });
+var messageTextbox =  jQuery('[name=message]')
 
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
     
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function(){
-        
+        messageTextbox.val('')
     });
 });
 
@@ -43,13 +44,18 @@ locationButton.on('click', function(){
    if(!navigator.geolocation){
        return alert('Geolocation not supported by your browser.')
    } 
+   
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Send location');
        socket.emit('createLocationMessage', {
            latitude: position.coords.latitude,
            longitude: position.coords.longitude
        });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
+        
     });
     
 });
